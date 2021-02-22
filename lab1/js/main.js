83e7062ab552dd1fb4c3af9070584e28008d2b37
drewmelt <drewmelt132@gmail.com>
2021-02-22 18:53:09 +0300

    refactor

--- lab1/js/main.js
+++ lab1/js/main.js
@@ -1,12 +1,10 @@
 const option = {
-    bezierCurve: false,
     maintainAspectRatio: false,
     elements: {
         point: {
-            radius: 1
+            radius: 0
         }
     },
-    responsive: true,
     scales: {
         yAxes: [{
             stacked: true,
@@ -19,17 +17,14 @@ const option = {
             gridLines: {
                 display: false
             }
-        }],
+        }]
     }
 };
 
 const conf = (name, len, arr) => ({
     type: 'line',
     data: {
-        labels: new Array(len)
-            .fill(0)
-            .map((_, i) => i),
-
+        labels: new Array(len).fill(0).map((_, i) => i),
         datasets: [{
             lineTension: 0,
             label: name,
@@ -43,9 +38,9 @@ const conf = (name, len, arr) => ({
     options: option,
 })
 
-const newGraphic = (color, arr) => ({
+const newGraphic = (color, name, arr) => ({
     lineTension: 0,
-    label: '2.1',
+    label: name,
     backgroundColor: window.chartColors = '#fff',
     borderColor: window.chartColors = color,
     borderWidth: 2,
@@ -58,38 +53,25 @@ const harmonicsAmount = 8
 const frequency = 1100
 const N = 256
 
-const main = () => {
-    const res = []
-    for (let i = 0; i < harmonicsAmount; i++) {
-        let W = (frequency / harmonicsAmount) * (i + 1)
-        let amplitude = Math.random()
-        let phase = Math.random()
-        for (let j = 0; j < N; j++) {
-            res[j] = (amplitude * Math.sin(W * j + phase))
-        }
-    }
-    return res
-}
-
+const createArray = len => new Array(len).fill(0)
 const average = arr => arr.reduce((a, c) => a + c) / arr.length
 const mathAverageTrick = arr => Math.sqrt(average(arr.map(num => Math.pow((num - average(arr)), 2))))
+
+const main = () => {
+    let array = createArray(N)
+    const harmArray = createArray(harmonicsAmount)
+    harmArray.forEach((_, i) => array = array.map((item, j) => item + (Math.random() * Math.sin(((frequency / harmonicsAmount) * (i + 1)) * j + Math.random()))))
+    return array
+}
 
 const correlation = (sig1, sig2) => {
     const a1 = average(sig1)
     const a2 = average(sig2)
     const mat1 = mathAverageTrick(sig1)
     const mat2 = mathAverageTrick(sig2)
-    const ans = []
-    const len = Math.floor(sig1.length / 2)
-
-    for (let i = 0; i < len; i++) {
-        let cov = 0
-        for (j = 0; j < len; j++) {
-            cov += (sig1[j] - a1) * (sig2[i + j] - a2) / (len - 1)
-        }
-        ans.push(cov / (Math.sqrt(mat1) * Math.sqrt(mat2)))
-    }
-    return ans
+    const len = parseInt(sig1.length / 2)
+    const array = createArray(len)
+    return array.map((_, i) => (array.reduce((a, _, j) => a + (sig1[j] - a1) * (sig2[i + j] - a2) / (len - 1))) / (Math.sqrt(mat1) * Math.sqrt(mat2)))
 }
 
 const autoCorrelation = sig => correlation(sig, sig)
@@ -104,16 +86,16 @@ window.onload = function() {
     const sig2 = main()
 
     const graphic = new Chart(ctx, conf('1.0', N, sig1))
+    console.log('Середнє значення', average(sig1))
+    console.log('Дисперсія', mathAverageTrick(sig1))
 
     const secConf = conf('2.0', N / 2, autoCorrelation(sig1))
-
-    secConf.data.datasets.push(newGraphic('red', sig1))
+    secConf.data.datasets.push(newGraphic('red', '2.1', sig1))
     const graphic2 = new Chart(ctx2, secConf)
 
 
-    const thirdConf = conf('3.0', N / 2, correlation(main(), main()))
-    thirdConf.data.datasets.push(newGraphic('red', sig1))
-    thirdConf.data.datasets.push(newGraphic('orange', sig2))
+    const thirdConf = conf('3.0', N / 2, correlation(sig1, sig2))
+    thirdConf.data.datasets.push(newGraphic('red', '3.1', sig1))
+    thirdConf.data.datasets.push(newGraphic('orange', '3.2', sig2))
     const graphic3 = new Chart(ctx3, thirdConf)
-
 }
